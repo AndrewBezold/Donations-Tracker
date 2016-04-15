e9f0042b1d404e59ad1b903e25b3ba442800e765
Andrew Bezold <andrew.bezold@gmail.com>
2016-04-15 14:20:29 -0400

    Multiple accounts
    Checks amount from a series of accounts in a list and adds them together

--- tracker.py
+++ tracker.py
@@ -1,17 +1,26 @@
 import requests
 import json
-id = "0xb794f5ea0ba39494ce839613fffba74279579268"
-# loop through transactions of account until field data is empty
-check = False
-iterator = 0
-amount = 0
-while(check == False):
-	account = requests.get("https://etherchain.org/api/account/" + id + "/tx/" + str(iterator))
-	json_text = json.loads(account.text)
-	if(len(json_text["data"]) == 0):
-		check = True
-	else:
-		for i in range(len(json_text["data"])):
-			amount += json_text["data"][i]["amount"]
-		iterator += 1
-print (amount)
+
+def getAmountFromAccountEther(id):
+	# loop through transactions of account until field data is empty
+	check = False
+	iterator = 0
+	amount = 0
+	while(check == False):
+		account = requests.get("https://etherchain.org/api/account/" + id + "/tx/" + str(iterator))
+		json_text = json.loads(account.text)
+		if(len(json_text["data"]) == 0):
+			check = True
+		else:
+			for i in range(len(json_text["data"])):
+				amount += json_text["data"][i]["amount"]
+			iterator += 1
+	return amount
+
+def getAmountFromMultipleAccountsEther(ids):
+	#loop through accounts in ids, adding amount from each
+	amount = 0
+	for i in range(len(ids)):
+		amount += getAmountFromAccountEther(ids[i])
+	return amount
+
