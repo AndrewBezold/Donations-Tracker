9d63af874c152139fc7b5145d20a6fd90f513781
Andrew Bezold <andrew.bezold@gmail.com>
2016-04-18 01:31:21 -0400

    JSON Testing
    Added functionality to test new JSON functions

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import tracker
+import alterJson
 
 id = "0xb794f5ea0ba39494ce839613fffba74279579268"
 ids = ["0xb794f5ea0ba39494ce839613fffba74279579268", "0x60e16961ad6138d2fb3e556fc284d9c2fff41486"]
 
 print(tracker.getAmountFromAccountEther(id))
 print(tracker.getAmountFromMultipleAccountsEther(ids))
+print(alterJson.addNonprofit("testProgram"))
+print(alterJson.addAddress("testProgram", "testid"))
+print(alterJson.addAddress("newTest", "testid2"))
